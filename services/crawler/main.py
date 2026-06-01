2c276cadd7b0cc2d855ac23b4d62513ce13c4965
lgthevinh <everwellmax@gmail.com>
2026-06-01 11:56:18 +0700

    feat(pipeline): Enable CSV loading into staging for pipeline processing

--- services/crawler/main.py
+++ services/crawler/main.py
@@ -9,14 +9,14 @@ BASEOUTPUT_DIR = Path(__file__).resolve().parent / "output"
 
 
 def main() -> None:
-    fpt_crawler = FPTShopCrawler(output_dir=str(BASEOUTPUT_DIR))
-    fpt_crawler.crawl()
+    # fpt_crawler = FPTShopCrawler(output_dir=str(BASEOUTPUT_DIR))
+    # fpt_crawler.crawl()
 
     # phongvu_crawler = PhongVuCrawler(output_dir=str(BASEOUTPUT_DIR))
     # phongvu_crawler.crawl()
     
-    # cellphones_crawler = CellphonesCrawler(output_dir=str(BASEOUTPUT_DIR))
-    # cellphones_crawler.crawl()
+    cellphones_crawler = CellphonesCrawler(output_dir=str(BASEOUTPUT_DIR))
+    cellphones_crawler.crawl()
 
 
 if __name__ == "__main__":
